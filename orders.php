599da84b773bb509f6c2302b9cc3f9e8487ce10a
Nekoza <nguyenminhhoangvu2005@gmail.com>
2025-05-04 10:54:40 +0700

    implement view performance and refund

--- orders.php
+++ orders.php
@@ -61,10 +61,10 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h2>Contact Us</h2>
+                    <h2>Wishlist</h2>
                     <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active"> Contact Us </li>
+                        <li class="breadcrumb-item"><a href="#">Shop</a></li>
+                        <li class="breadcrumb-item active">Wishlist</li>
                     </ul>
                 </div>
             </div>
@@ -72,43 +72,59 @@
     </div>
     <!-- End All Title Box -->
 
-    <!-- Start Contact Us  -->
-    <div class="contact-box-main">
+    <!-- Start Wishlist  -->
+    <div class="wishlist-box-main">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 col-sm-12">
-                    <div class="contact-form-right">
-                        <h2>GET IN TOUCH</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed odio justo, ultrices ac nisl sed, lobortis porta elit. Fusce in metus ac ex venenatis ultricies at cursus mauris.</p>
-                        <form id="contactForm">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <select name="type" id="" class="form-control">
-                                            <option value="Refund" selected>Refund</option>
-                                        </select>
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <textarea class="form-control" id="message" placeholder="Your Message" rows="4" data-error="Write your message" required></textarea>
-                                        <div class="help-block with-errors"></div>
-                                    </div>
-                                    <div class="submit-button text-center">
-                                        <button class="btn hvr-hover" id="submit" type="button">Send Message</button>
-                                        <div id="msgSubmit" class="h3 text-center hidden"></div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+                <div class="col-lg-12">
+                    <div class="table-main table-responsive" style="overflow: hidden">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th>OrderID</th>
+                                    <th>Product Name</th>
+                                    <th>Amount </th>
+                                    <th>Status</th>
+                                    <th>Track Order</th>
+                                    <th>Refund</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td class="thumbnail-img">
+                                        1
+                                    </td>
+
+                                    <td class="name-pr">
+                                        <ul style="list-style: square">
+                                            <li><a href="">- Golden Blossoms</a></li>
+                                            <li><a href="">- Rose</a></li>
+                                        </ul>
+                                    </td>
+                                    
+                                    <td class="price-pr">
+                                        <p>$ 80.0</p>
+                                    </td>
+
+                                    <td class="quantity-box">Purchased</td>
+
+                                    <td class="add-pr">
+                                        <a class="btn hvr-hover" href="#">Track Order</a>
+                                    </td>
+
+                                    <td class="add-pr">
+                                        <a class="btn hvr-hover" href="./contact-us.php?orderID=1">Refund Order</a>
+                                    </td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- End Cart -->
+    <!-- End Wishlist -->
 
     <!-- Start Instagram Feed  -->
     <!-- End Instagram Feed  -->
@@ -126,36 +142,6 @@
     <!-- End copyright  -->
 
     <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
-
-    <script>
-        const form = document.querySelector('form')
-        const btn = document.querySelector('form button')
-        const requestType = document.querySelector('form select')
-        const requestReason = document.querySelector('form textarea')
-        const params = new URLSearchParams(document.location.search)
-        const orderID = parseInt(params.get('orderID', 10))
-
-        form.onsubmit = (e) => {
-            e.preventDefault()
-        } 
-
-        btn.onclick = async (e) => {
-            const formData = new FormData()
-            formData.append('type', requestType.value)
-            formData.append('reason', requestReason.value)
-            formData.append('orderID', orderID)
-
-            const res = await fetch('./controller/request_controller.php', {
-                method: 'POST',
-                body: formData
-            })
-
-            const data = await res.json()
-            console.log(data)
-        }
-
-
-    </script>
 
     <!-- ALL JS FILES -->
     <script src="js/jquery-3.2.1.min.js"></script>
